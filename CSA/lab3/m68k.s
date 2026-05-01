e66b6072c06f359b7de0d51a96c8098f9c172e4f
Timofey Kirichenko <timofeykirichenko@gmail.com>
2026-05-02 01:05:44 +0300

    ⚡⚡⚡m68k - ВСЁ ⚡⚡⚡

--- CSA/lab3/m68k.s
+++ CSA/lab3/m68k.s
@@ -74,93 +74,88 @@ _start:
 
 ; A6 - Stack
 main:
-    ; 0..63   - byte in_buff[64]
-    ; 64..127 - byte out_buff[64]
+    ; 0..65   - byte in_buff[64]
+    ; 66..128 - byte out_buff[64]
     link A6, -128
 
     move.l -65, D7                        
     jsr    read_input
 
-    
-
     ; D0 - prev char
     ; D1 - prev char_counter
-    ; D2 - result_size
+    ; D2 - out_buf_idx
     move.b 1, D1
     move.b 0, D2
     move.b -65(A6), D0
-    beq    exit
 
-     
+    ; if in_buff.len == 0 {exit}
+    beq    m_exit
+
     ; Main compression algorithm
     ; D7 - Line iter, started from i=1
-    ; D3 - Current char
     move.l -64, D7
     m_loop:
-    ; ch = read()
-    move.b 0(A6, D7), D3
-    ; if ch == 0 {break;}
-    beq    m_loop_exit
+        ; D3 - current char; ch = read()
+        move.b 0(A6, D7), D3
+        ; if ch == 0 {break;}
+        beq    m_loop_exit
 
-    ; ch = line[i]
-    ; if ch != p_ch || cnt == 9:
-    ;     if print_cnt == 62:
-    ;         exit(0xCCCCCC)
-    ;     else:
-    ;         print(cht .. ch)
-    ; else:
-    ;     cnt += 1
-    
-    cmp.b D3, D0
-    bne m_new_char
-    cmp.b 9, D1
-    beq m_new_char
-    add.b 1, D1
-    jmp m_new_char_over
+        ; if ch != prev_ch || cnt == 9 {goto m_new_char;}
+        cmp.b D3, D0
+        bne   m_new_char
+        cmp.b 9, D1
+        beq   m_new_char
 
-    m_new_char:
-    ; Store pair
-    move.b D1, -128(A6, D2)
-    move.b D0, -127(A6, D2)
-    ; Update prev_char and counter
-    move.b 1, D1
-    move.b D3, D0
-    ; Shift and check output buffer
-    add.b 2, D2
-    cmp.b 62, D2 ; reserve 2 bytes to end
-    beq overflow_exit
-    m_new_char_over:
-    
-    add.l 1, D7
-    jmp m_loop
+        ; else {cnt += 1; continue;}
+        add.b 1, D1
+        jmp m_loop_continue
+        
+        m_new_char:
+            ; Store pair
+            move.b D1, -129(A6, D2)
+            move.b D0, -128(A6, D2)
+            ; Update prev_char and counter
+            move.b 1, D1
+            move.b D3, D0
+            ; out_buff_idx += 2
+            add.b  2, D2                          
+            ; if out_buff_idx >= 62 {overflow_exit}    ; reserve 2 bytes for the last pair
+            cmp.b  62, D2                         
+            beq overflow_exit
+
+    m_loop_continue:        
+        add.l 1, D7
+        jmp m_loop
     m_loop_exit:
 
-    ; Add last pair
-    move.b D1, -128(A6, D2)
-    move.b D0, -127(A6, D2)
-    add.b 2, D2
+    ; Store the last pair
+    move.b D1, -129(A6, D2)
+    move.b D0, -128(A6, D2)
+    add.b  2, D2
 
-    ; Print pairs
+    ; drop D0, D1, D7, D3
+
     ; A5 - output addr
     movea.l output_addr, A5
     movea.l (A5), A5
 
-    ; movea.l A6, A5
-
-    ; Output buffer iter
+    ; Print pairs
+    ; D3 - out_buff_iter aka i
     move.b 0, D3  
     m_print_loop:
-    ; while iter != buf_len
-    cmp.b D2, D3
-    beq m_print_loop_exit
+        ; if (iter == buf_len) {break;}
+        cmp.b D2, D3
+        beq m_print_loop_exit
 
-    move.b -128(A6, D3), D0
-    add.b '0', D0
-    move.b D0, (A5)
-    move.b -127(A6, D3), (A5)
+        ; print(out_buff[i] + 0)
+        ; print(out_buff[i])
+        move.b -129(A6, D3), D0
+        add.b '0', D0
+        move.b D0, (A5)
+        move.b -128(A6, D3), (A5)
 
-    add.b 2, D3 ; Shift iter
-    jmp m_print_loop
+        add.b 2, D3
+        jmp m_print_loop
     m_print_loop_exit:
 
     m_exit:
@@ -210,8 +205,6 @@ read_input:
 
     rts
 
-exit:
-    halt
 
 overflow_exit:
     movea.l output_addr, A5
